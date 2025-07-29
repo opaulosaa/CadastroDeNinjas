4acacf66d3ab5a72e32b84c1d0544f3ce4ff41dc
opaulosaa <pauloarneto@gmail.com>
2025-07-29 14:43:08 -0300

    primeira rota adicionada

---
+++ src/main/java/dev/paulosa/CadastroDeNinjas/Controller.java
@@ -0,0 +1,17 @@
+package dev.paulosa.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Essa é a primeira mensagem nessa rota";
+    }
+
+
+}
