a703543f3a93e51da38db0692ef495db486b72f7
opaulosaa <pauloarneto@gmail.com>
2025-07-30 00:01:21 -0300

    feat: Coloca o lombok na classe para abstracao

--- src/main/java/dev/paulosa/CadastroDeNinjas/Missoes/Model/MissaoModel.java
+++ src/main/java/dev/paulosa/CadastroDeNinjas/Missoes/Model/MissaoModel.java
@@ -2,13 +2,18 @@ package dev.paulosa.CadastroDeNinjas.Missoes.Model;
 
 import dev.paulosa.CadastroDeNinjas.Ninjas.Model.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissaoModel {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/dev/paulosa/CadastroDeNinjas/NinjaModel.java
+++ src/main/java/dev/paulosa/CadastroDeNinjas/NinjaModel.java
@@ -1,50 +1,27 @@
 package dev.paulosa.CadastroDeNinjas;
 
+import dev.paulosa.CadastroDeNinjas.Missoes.Model.MissaoModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String nome;
-    String email;
-    int idade;
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
 
-    public NinjaModel() {
-    }
+    @ManyToOne
+    @JoinColumn(name = "missoes_id")
+    private MissaoModel missoes;
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 
 }
