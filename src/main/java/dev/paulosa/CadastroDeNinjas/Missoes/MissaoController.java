758c8d0d2d988418842898fcb029facd2446b31c
opaulosaa <pauloarneto@gmail.com>
2025-08-01 11:05:49 -0300

    feat: Cria as rotas do controller com mock

---
+++ src/main/java/dev/paulosa/CadastroDeNinjas/Missoes/MissaoController.java
@@ -0,0 +1,29 @@
+package dev.paulosa.CadastroDeNinjas.Missoes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissaoController {
+
+    @GetMapping("/listar")
+    public String listarMissao(){
+        return "Missoes listadas";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão criada";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "missao alterada";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada";
+    }
+
+}
